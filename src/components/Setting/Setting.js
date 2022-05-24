576ad106d3ce1f76233e38b471d89ac6cadf41f9
Mohamed Elkamary <mohamedelkamary5@gmail.com>
2022-05-24 15:51:21 +0200

    done items

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import AdminsAlerts from './adminsAlerts/adminsAlerts';
-import AdminsPermissions from './adminsPermissions/adminsPermissions';
+import AdminsAlerts from './AdminsAlerts/AdminsAlerts';
+import AdminsPermissions from './AdminsPermissions/AdminsPermissions';
 import './setting.scss'
 
 
@@ -15,17 +15,21 @@ const SettingTabs = () => {
         <div className='row'>
           <div className='col-lg-3 p-lg-0 mt-lg-5 pt-lg-3'>
             <div className="nav nav-tabs" id="nav-tab" role="tablist">
-              <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">التنبيهات</button>
-              <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">المشرفين</button>
+              <button className="nav-link active" id="nav-alerts-tab" data-bs-toggle="tab" data-bs-target="#nav-alerts" role="tab" aria-controls="nav-alerts" aria-selected="true">التنبيهات</button>
+              <button className="nav-link" id="nav-permissions-tab" data-bs-toggle="tab" data-bs-target="#nav-permissions" role="tab" aria-controls="nav-permissions" aria-selected="false">المشرفين</button>
             </div>
           </div>
 
           <div className="col-lg-9 p-lg-0">
             <div className="tab-content" id="nav-tabContent">
-              <AdminsPermissions />
-              <AdminsAlerts />
+              <div className="tab-pane fade show active" id="nav-alerts" role="tabpanel" aria-labelledby="nav-alerts-tab" tabIndex={0}>
+                <AdminsAlerts />
+              </div>
+              
+              <div className="tab-pane" id="nav-permissions" role="tabpanel" aria-labelledby="nav-permissions-tab">
+                <AdminsPermissions />
+              </div>
             </div>
-            
           </div>
         </div>
 
@@ -35,53 +39,3 @@ const SettingTabs = () => {
 }
 
 export default SettingTabs;
-
-
-// import { Tabs, Radio } from 'antd';
-
-// const { TabPane } = Tabs;
-
-// class Demo extends React.Component {
-//   state = { size: 'small' };
-
-//   onChange = e => {
-//     this.setState({ size: e.target.value });
-//   };
-
-//   render() {
-//     const { size } = this.state;
-//     return (
-//       <div>
-//         <Radio.Group value={size} onChange={this.onChange} style={{ marginBottom: 16 }}>
-//           <Radio.Button value="small">Small</Radio.Button>
-//           <Radio.Button value="default">Default</Radio.Button>
-//           <Radio.Button value="large">Large</Radio.Button>
-//         </Radio.Group>
-//         <Tabs defaultActiveKey="1" size={size} style={{ marginBottom: 32 }}>
-//           <TabPane tab="Tab 1" key="1">
-//             Content of tab 1
-//           </TabPane>
-//           <TabPane tab="Tab 2" key="2">
-//             Content of tab 2
-//           </TabPane>
-//           <TabPane tab="Tab 3" key="3">
-//             Content of tab 3
-//           </TabPane>
-//         </Tabs>
-//         <Tabs defaultActiveKey="1" type="card" size={size}>
-//           <TabPane tab="Card Tab 1" key="1">
-//             Content of card tab 1
-//           </TabPane>
-//           <TabPane tab="Card Tab 2" key="2">
-//             Content of card tab 2
-//           </TabPane>
-//           <TabPane tab="Card Tab 3" key="3">
-//             Content of card tab 3
-//           </TabPane>
-//         </Tabs>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Demo;
--- src/components/Setting/adminsPermissions/adminsPermissions.js
+++ src/components/Setting/adminsPermissions/adminsPermissions.js
@@ -1,18 +1,22 @@
+
+
 import React, {useState} from 'react';
 import SwitchButton from '../../../Shared/Components/Switch/SwitchButton';
 
 
-const AdminsPermissions = () => {
+const AdminsAlerts = () => {
 
   const initialPermissions = {
     all: false,
-    contactAdmin: false,
-    CustomerLatePayment: false,
-    payingSubscriptions: false,
-    changeCustomerInformation: false,
-    addNewCustomer: false,
+    addNewClient: false,
+    editClientAccounts: false,
     stopClient: false,
     deleteClient: false,
+    reactivateClients: false,
+    viewClientActivityHistory: false,
+    deleteClient: false,
+    viewClientActivityHistory: false,
+    
     
   }
 
@@ -21,76 +25,105 @@ const AdminsPermissions = () => {
 
     const handleChange = (e, name) => {
       if (name === 'all') {
-        
+        setChecked({
+          ...checked, 
+          [name]: !e['all'],
+          addNewClient: !e['all'],
+          editClientAccounts: !e['all'],
+          stopClient: !e['all'],
+          deleteClient: !e['all'],
+          reactivateClients: !e['all'],
+          // addNewClient: !e['all'],
+          // viewClientActivityHistory: !e['all'],
+          // deleteClient: !e['all'],
+          viewClientActivityHistory: !e['all'],
+        });
       } else{
-        setChecked({...checked, [name]: !e[name]});
+        setChecked({
+          ...checked,
+          [name]: !e[name],
+          all: false
+        });
+
       }
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('checked', checked);
     }
 
 
     return (
-        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabIndex={0}>
-                <form>
-                  <div className='row'>
-                    <div className='col-lg-6'> 
-                      <h4>تعديل التنبيهات الواردة</h4>
-                    </div>
-                    <div className='col-lg-6'>
-                      <div className='d-flex justify-content-center'>
-                        <button className='btn btn-success btn-lg px-lg-5'>حفظ التعديل</button>
-                        <button  className="btn btn-link text-dark">إرجاع للوضع الافتراضي</button>
-                      </div>
-                    </div>
-                  </div>
+      <form onSubmit={handleSubmit}>
+        <div className='row'>
+          <div className='col-lg-6'> 
+            <h4>تعديل سلطات المشرفين الافتراضيه</h4>
+          </div>
+          <div className='col-lg-6'>
+            <div className='d-flex justify-content-center'>
+              <button className='btn btn-success btn-lg px-lg-5'>حفظ التعديل</button>
+              <button  className="btn btn-link text-dark">إرجاع للوضع الافتراضي</button>
+            </div>
+          </div>
+        </div>
 
-                  <div className="row mt-lg-5">
-                    <div className="col-lg-6">
-                    
-                      <SwitchButton id='all' 
-                        value={checked['all']} 
-                        onChange={() => handleChange(checked, 'all')} label='تحديد الكل:' 
-                      />
-                      <SwitchButton id='contactAdmin'
-                        value={checked['contactAdmin']} 
-                        onChange={() => handleChange(checked, 'contactAdmin')} label='تنبيه عند إتصال مشرف:' 
-                      />
-                      <SwitchButton id='CustomerLatePayment'
-                        value={checked['CustomerLatePayment']} 
-                        onChange={() => handleChange(checked, 'CustomerLatePayment')} label='تنبيه لتأخر دفع العميل:' 
-                      />
-                      <SwitchButton id='payingSubscriptions'
-                        value={checked['payingSubscriptions']} 
-                        onChange={() => handleChange(checked, 'payingSubscriptions')} label='تنبيه عند دفع الاشتراكات:' 
-                      />
-                      
-                      <h5 className='my-3'> :تنبيه عن اي تغير عن طريق مشرف في </h5>
-
-                      <SwitchButton id='changeCustomerInformation' 
-                        value={checked['changeCustomerInformation']} 
-                        onChange={() => handleChange(checked, 'changeCustomerInformation')} label='تغير معلومات العميل:' 
-                      />
-                      <SwitchButton id='addNewCustomer'
-                        value={checked['addNewCustomer']} 
-                        onChange={() => handleChange(checked, 'addNewCustomer')} label='إضافه عميل جديد:' 
-                      />
-                      <SwitchButton id='stopClient'
-                        value={checked['stopClient']} 
-                        onChange={() => handleChange(checked, 'stopClient')} label='إيقاف عميل:' 
-                      />
-                      <SwitchButton id='deleteClient'
-                        value={checked['deleteClient']} 
-                        onChange={() => handleChange(checked, 'deleteClient')} label='حذف عميل:' 
-                      />
+        <div className="row mt-lg-5">
+          <div className="col-lg-6">
+          
+            <SwitchButton id='all' 
+              value={checked['all']} 
+              onChange={() => handleChange(checked, 'all')} label='تحديد الكل:' 
+            />
+            <SwitchButton id='addNewClient'
+              value={checked['addNewClient']} 
+              onChange={() => handleChange(checked, 'addNewClient')} label='أضافه العملاء الجدد:' 
+            />
+            <SwitchButton id='editClientAccounts'
+              value={checked['editClientAccounts']} 
+              onChange={() => handleChange(checked, 'editClientAccounts')} label=' :تعديل حسابات العملاء:' 
+            />
+            <SwitchButton id='stopClient'
+              value={checked['stopClient']} 
+              onChange={() => handleChange(checked, 'stopClient')} label=' :إقاف العملاء:' 
+            />                
+            <SwitchButton id='deleteClient' 
+              value={checked['deleteClient']} 
+              onChange={() => handleChange(checked, 'deleteClient')} label=' :حذف العملاء:' 
+            />
+            <SwitchButton id='reactivateClients'
+              value={checked['reactivateClients']} 
+              onChange={() => handleChange(checked, 'reactivateClients')} label='أعاده تفعيل العملاء بعد الاقاف/ الحذف:' 
+            />
+            <SwitchButton id='viewClientActivityHistory'
+              value={checked['viewClientActivityHistory']} 
+              onChange={() => handleChange(checked, 'viewClientActivityHistory')} label='مشاهده سجل نشاط العملاء:' 
+            />
 
 
-                    </div>
-                    <div className="col-lg-6">
-                      
-                    </div>
-                  </div>
-                </form>
+          </div>
+          <div className="col-lg-6">
+            <div className='flex-center h-100'>
+              <div className='text-center px-lg-5'>
+                <svg className='mb-4' xmlns="http://www.w3.org/2000/svg" width="61.953" height="61.953" viewBox="0 0 61.953 61.953">
+                <g id="Group_671" data-name="Group 671" transform="translate(1.5 1.5)">
+                  <path id="Path_154" data-name="Path 154" d="M61.953,32.477A29.477,29.477,0,1,1,32.477,3,29.477,29.477,0,0,1,61.953,32.477Z" transform="translate(-3 -3)" fill="none" stroke="#6e6e6e" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} />
+                  <path id="Path_155" data-name="Path 155" d="M18,29.791V18" transform="translate(11.477 11.477)" fill="none" stroke="#6e6e6e" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} />
+                  <path id="Path_156" data-name="Path 156" d="M18,12h0" transform="translate(11.477 5.686)" fill="none" stroke="#6e6e6e" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} />
+                </g>
+              </svg>
+              <p>
+               يمكنك تعديل سلطات المشرفين الافتراضيه لكل المشرفين
+               و سوف تغير المشرفين اللذين على وضع السلطات الافتارضيه
+               فقط و لن تعدل اللذي عٌ دلوا بصوره يدوية
+              </p>
               </div>
+
+            </div>    
+          </div>
+        </div>
+      </form>
     );
 }
 
-export default AdminsPermissions;
+export default AdminsAlerts;
