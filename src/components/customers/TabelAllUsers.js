703a720ad40b0af006b4fdcb89b92295d201362f
Mohamed Elkamary <mohamedelkamary5@gmail.com>
2022-06-10 06:57:39 +0200

    style table

--- src/components/customers/TabelAllUsers.js
+++ src/components/customers/TabelAllUsers.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 import Logo3 from "../../photo/slogan/user-avatar.svg"
 
-const TabelAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
+const TableAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
      const UserData = useSelector(state => state.clients2.clients2)
     
     const dispatch = useDispatch();
@@ -45,7 +45,7 @@ const TabelAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
     
 
   return (
-  <MainTabel>
+  <div className="main-table">
       <SortTabel 
        setSortSearch={setSortSearch} 
        searchSort={searchSort} 
@@ -57,9 +57,9 @@ const TabelAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
        sortName={sortName} 
        sortDuration={sortDuration}
        sortpaymentDate={sortpaymentDate}  />
-    <Tabel>
-        <Thead>
-            <TrHead>
+    <table>
+        <thead>
+            <tr>
                 <th >الشعار</th>
                 <th >#</th>
                 <th> الأسم بالانجليزي</th>
@@ -71,9 +71,9 @@ const TabelAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
                 <th>الحالة</th>
                 <th>شركة الشحن</th>
                 <th>الخيارات</th>
-            </TrHead>
-        </Thead>
-        <Tbody>
+            </tr>
+        </thead>
+        <tbody>
         {sortedField.filter((item)=>{
             if (searchSort === "") {
                 return item
@@ -82,7 +82,7 @@ const TabelAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
             }
         }).map((user , index) =>{
             return (
-                <TrBody key={index}>
+                <tr key={index}>
                     <td><Link className='my-2' to={`/restaurants/${user.id}`}><img src={Logo3} alt="logo" /></Link></td>
                     <td>
                         <Link className='text-link' to={`/restaurants/${user.id}`}>{user.id}#</Link>
@@ -112,155 +112,16 @@ const TabelAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
                          <Link to={`/shipping-companies/${user.user.id}`}>شركة الشحن</Link>
                      </div>
                     </td>
-                </TrBody>
+                </tr>
             )
         })}
-        </Tbody>
-    </Tabel>
+        </tbody>
+    </table>
 
-  </MainTabel> 
+  </div> 
   )
 }
-const MainTabel = styled.div`
-    overflow-x: auto;
-     width:100% ;
-     height: 84vh;
-    padding:  50px;
-    
-
-    @media (max-width:1180px) {
-        padding: 0 0px;
-    }
-     &::-webkit-scrollbar{
-        width: 0;   
-    }
 
 
-`
-const Tabel = styled.table`
-    text-align: center;
-    border: 0px;
-    border-spacing: 0px 20px;
-    align-items: center;
-    width:100% ;
-    margin: 0;
-    padding: 0;
-    @media (max-width:1180px) {
-        padding: 0 10px;
-    }
-    @media (max-width:770px ) {
-     margin-top: 10px;
-     padding: 0 0px;
-    }
-  
 
-`
-const Thead = styled.thead`
-background-color:var(--secound-color);
-color: white;
-
-`
-const TrHead = styled.tr`
-
-th{
-    padding: 10px 25px;
-    @media (max-width:720px) {
-     font-size: 13px;
-     padding: 10px 40px;
-    }
-}
-`
-const Tbody = styled.tbody`
-    border-radius: 10px;
-    
-`
-const TrBody= styled.tr`
-padding: 10px 5px;
-border-top: 10px solid #ebebebe6;
-
-td {
-    background-color: white;    
-    
-    color: var(--font);
-    font-size: 17px;
-    &:first-child{
-        border-radius: 0px 10px 10px 0;
-    }
-    &:last-child{
-        border-radius: 10px 0px 0px 10px;
-    }
-    img{
-        width: 60px;
-        height: 60px;
-        border-radius: 50%;
-    }
-    .BiDotsHorizontalRounded{
-        margin-top: 10px;
-        font-size: 30px;
-        cursor: pointer;
-
-    }
-    @media (max-width:720px) {
-     font-size: 15px;
-     
-    }
-}
-td:last-child{
-    position: relative;
-    &:hover{
-        .select-clint{
-            transform: scale(1);
-            transform-origin: 0 100% 100% 100% ;
-        }  
-    }
-    .select-clint{
-        box-shadow: -2px 5px 13px -4px rgba(0,0,0,0.46);
-        -webkit-box-shadow: -2px 5px 13px -4px rgba(0,0,0,0.40);
-        -moz-box-shadow: -2px 5px 13px -4px rgba(0,0,0,0.30);
-        position: absolute;
-        transform: scale(0);
-        transform-origin: 20% 40%;
-        left: 20px;
-        top: 40px;
-        z-index: 4;
-        width: 100px;
-        display: flex;
-        flex-direction: column;
-        background-color: white;
-        transition: 0.4s;    
-        a{
-            font-size: 14px;
-            font-weight: bold;
-            color: var(--font);
-            cursor: pointer;
-            padding: 5px 0;
-            border-bottom: 1px solid black;
-            &:last-child{
-                border-bottom: none;
-            }
-
-        }
-    }
-}
-
-
-.red{
-    color: var(--danger-color);
-    font-weight: bold;
-    background-color: #ff000015;
-    padding: 10px;
-    border-radius:30px ;
-        
-    }
-.green{
-    color: var(--succes-color);
-    font-weight: bold;
-    background-color: #00800015;
-    padding: 10px;
-    border-radius:30px ;
-}
-
-`
-
-
-export default TabelAllUsers
+export default TableAllUsers
