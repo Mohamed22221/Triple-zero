b70d98bcf20ab9c6f12b881c537291903e18c869
Mohamed Elkamary <mohamedelkamary5@gmail.com>
2022-06-10 17:01:06 +0200

    done black list

--- src/components/restaurants/TabelBlackList.js
+++ src/components/restaurants/TabelBlackList.js
@@ -1,21 +1,23 @@
-import React , {useState ,useEffect} from 'react'
-import styled from "styled-components"
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { BiDotsHorizontalRounded } from 'react-icons/bi';
 import SortTabel from './SortTabel';
-import {getClients2} from '../../store/ClintSlice2'
+import { getClients2 } from '../../store/ClintSlice2'
 import { Link } from "react-router-dom";
 
 import Logo3 from "../../photo/slogan/user-avatar.svg"
+import Logo1 from "../../photo/slogan/logo-rest.png"
 
-const TableAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
-     const UserData = useSelector(state => state.clients2.clients2)
-     console.log('UserData', UserData);
- 
+const TableAllUsers = ({ searchSort, setSortSearch, HandelShowCustomer }) => {
+    const UserData = useSelector(state => state.clients2.clients2)
+    const listView = useSelector(state => state.clients2.listView)
+
     const dispatch = useDispatch();
     useEffect(() => {
-    dispatch(getClients2())
+        dispatch(getClients2())
     }, [dispatch])
+
+
 
 
     //sort tabel 
@@ -24,239 +26,158 @@ const TableAllUsers = ({searchSort , setSortSearch ,HandelShowCustomer }) => {
         setSortedField(UserData)
     }, [UserData, setSortedField])
 
-    const sortID = [...UserData].sort((a , b)=>{
+    const sortID = [...UserData].sort((a, b) => {
         return a.id < b.id ? 1 : -1;
     })
-    const sortData = [...UserData].sort((a , b)=>{
+    const sortData = [...UserData].sort((a, b) => {
         return a.state > b.state ? 1 : -1;
     })
-    const sortName = [...UserData].sort((a , b)=>{
+    const sortName = [...UserData].sort((a, b) => {
         return a.en_name < b.en_name ? 1 : -1;
     })
-    const sortDuration = [...UserData].sort((a , b)=>{
+    const sortDuration = [...UserData].sort((a, b) => {
         return a.duration > b.duration ? 1 : -1;
     })
-    const sortpaymentDate = [...UserData].sort((a , b)=>{
+    const sortpaymentDate = [...UserData].sort((a, b) => {
         const dateA = new Date(a.paymentDate), dateB = new Date(b.paymentDate)
         return dateB - dateA
     })
 
 
-  return (
-  <MainTable>
-      <SortTabel 
-       setSortSearch={setSortSearch} 
-       searchSort={searchSort} 
-       HandelShowCustomer={HandelShowCustomer}
-       UserData={UserData} 
-       setSortedField={setSortedField}
-       sortData={sortData} 
-       sortID={sortID} 
-       sortName={sortName} 
-       sortDuration={sortDuration}
-       sortpaymentDate={sortpaymentDate}  />
-    <Tabel>
-        <Thead>
-            <TrHead>
-                <th >الشعار</th>
-                <th >#</th>
-                <th> الأسم بالانجليزي</th>
-                <th>رقم التليفون</th>
-                <th>الايميل</th>
-                <th>العنوان</th>
-
-                
-                <th>الحالة</th>
-                <th>الخيارات</th>
-            </TrHead>
-        </Thead>
-        <Tbody>
-        {sortedField.filter((item)=>{
-            if (searchSort === "") {
-                return item
-            } else if (item.en_name.includes(searchSort)) {
-                return item
-            }
-        }).filter(statusItem => statusItem.status == 0 )
-        .map((user , index) =>{
-            return (
-                <TrBody key={index}>
-                    <td><Link className='my-2' to={`/restaurants/${user.id}`}><img src={Logo3} alt="logo" /></Link></td>
-                    <td>
-                        <Link className='text-link' to={`/restaurants/${user.id}`}>{user.id}#</Link>
-                    </td>
-                    <td>
-                        <span>{user.en_name}</span>
-                    </td>
-
-                    <td>
-                        <span>{user.mobile}</span>
-                    </td>
-                    <td>
-                        <span>{user.email}</span>
-                    </td>
-                    <td>
-                        <span>{user.address}</span>
-                    </td>
-
-                    <td ><span className='red'>غير نشط</span></td>
-                    <td >
-                     <BiDotsHorizontalRounded className='BiDotsHorizontalRounded'/>
-                     <div className='select-clint'>
-                         <Link to={`/restaurants/${user.id}`}>بيانات المطعم</Link>
-                         <Link to={`/shipping-companies/${user.user.id}`}>شركة الشحن</Link>
-                     </div>
-                    </td>
-                </TrBody>
-            )
-        })}
-        </Tbody>
-    </Tabel>
-
-  </MainTable> 
-  )
-}
-const MainTable = styled.div`
-    overflow-x: auto;
-     width:100% ;
-     height: 84vh;
-    padding:  50px;
-    
-
-    @media (max-width:1180px) {
-        padding: 0 0px;
-    }
-     &::-webkit-scrollbar{
-        width: 0;   
-    }
+    const dataRender = (
+        listView ?
+            <table>
+                <thead>
+                    <tr>
+                        <th >الشعار</th>
+                        <th >#</th>
+                        <th> الأسم بالانجليزي</th>
+                        <th>رقم التليفون</th>
+                        <th>الايميل</th>
+                        <th>العنوان</th>
 
 
-`
-const Tabel = styled.table`
-    text-align: center;
-    border: 0px;
-    border-spacing: 0px 20px;
-    align-items: center;
-    width:100% ;
-    margin: 0;
-    padding: 0;
-    @media (max-width:1180px) {
-        padding: 0 10px;
-    }
-    @media (max-width:770px ) {
-     margin-top: 10px;
-     padding: 0 0px;
-    }
-  
+                        <th>الحالة</th>
+                        <th>شركة الشحن</th>
+                        <th>الخيارات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sortedField.filter((item) => {
+                        if (searchSort === "") {
+                            return item
+                        } else if (item.en_name.includes(searchSort)) {
+                            return item
+                        }
+                    }).filter(statusItem => statusItem.status == 0 ).map((user, index) => {
+                        return (
+                            <tr key={index}>
+                                <td><Link className='my-2' to={`/restaurants/${user.id}`}><img src={Logo3} alt="logo" /></Link></td>
+                                <td>
+                                    <Link className='text-link' to={`/restaurants/${user.id}`}>{user.id}#</Link>
+                                </td>
+                                <td>
+                                    <span>{user.en_name}</span>
+                                </td>
 
-`
-const Thead = styled.thead`
-background-color:var(--secound-color);
-color: white;
+                                <td>
+                                    <span>{user.mobile}</span>
+                                </td>
+                                <td>
+                                    <span>{user.email}</span>
+                                </td>
+                                <td>
+                                    <span>{user.address}</span>
+                                </td>
 
-`
-const TrHead = styled.tr`
-
-th{
-    padding: 10px 30px;
-    @media (max-width:720px) {
-     font-size: 13px;
-     padding: 10px 40px;
-    }
-}
-`
-const Tbody = styled.tbody`
-    border-radius: 10px;
-    
-`
-const TrBody= styled.tr`
-padding: 10px 5px;
-border-top: 10px solid #ebebebe6;
-
-td {
-    background-color: white;    
-    
-    color: var(--font);
-    font-size: 17px;
-    &:first-child{
-        border-radius: 0px 10px 10px 0;
-    }
-    &:last-child{
-        border-radius: 10px 0px 0px 10px;
-    }
-    img{
-        width: 60px;
-        height: 60px;
-        border-radius: 50%;
-    }
-    .BiDotsHorizontalRounded{
-        margin-top: 10px;
-        font-size: 30px;
-        cursor: pointer;
-
-    }
-    @media (max-width:720px) {
-     font-size: 15px;
-     
-    }
-}
-td:last-child{
-    position: relative;
-    &:hover{
-        .select-clint{
-            transform: scale(1);
-            transform-origin: 0 100% 100% 100% ;
-        }  
-    }
-    .select-clint{
-        box-shadow: -2px 5px 13px -4px rgba(0,0,0,0.46);
-        -webkit-box-shadow: -2px 5px 13px -4px rgba(0,0,0,0.40);
-        -moz-box-shadow: -2px 5px 13px -4px rgba(0,0,0,0.30);
-        position: absolute;
-        transform: scale(0);
-        transform-origin: 20% 40%;
-        left: 20px;
-        top: 40px;
-        z-index: 4;
-        width: 100px;
-        display: flex;
-        flex-direction: column;
-        background-color: white;
-        transition: 0.4s;    
-        a{
-            font-size: 14px;
-            font-weight: bold;
-            color: var(--font);
-            cursor: pointer;
-            padding: 5px 0;
-            border-bottom: 1px solid black;
-            &:last-child{
-                border-bottom: none;
-            }
+                                <td ><span className={user.status === 1 ? "green" : "red"}>{user.status === 1 ? "نشط" : "غير نشط"}</span></td>
+                                <td>
+                                    <span>{user.user.ar_name}</span>
+                                </td>
+                                <td >
+                                    <BiDotsHorizontalRounded className='BiDotsHorizontalRounded' />
+                                    <div className='select-clint'>
+                                        <Link to={`/restaurants/${user.id}`}>التفاصيل</Link>
+                                        <Link to={`/shipping-companies/${user.user.id}`}>شركة الشحن</Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            :
+            <div className='row mt-2'>
+                {sortedField.filter((item) => {
+                    if (searchSort === "") {
+                        return item
+                    } else if (item.en_name.includes(searchSort)) {
+                        return item
+                    }
+                }).filter(statusItem => statusItem.status == 0 ).map((user, index) => {
+                    return (
+                        <div className='col-lg-3 mt-3'>
+                            <div className="card">
+                                <Link to={`/restaurants/${user.id}`} className='img-parent'>
+                                    <img src={Logo1} className="card-img-top" alt="..." />
+                                </Link>
+                                <div className="card-body text-center">
+                                    <h6 className="card-title mb-1">
+                                        <Link to={`/restaurants/${user.id}`}>{user.en_name}</Link>
+                                    </h6>
+                                    <p className='mt-2'>
+                                        <a href={`tel:+${user.mobile}`}>{user.mobile}</a>
+                                    </p>
+                                    <div className='row actions-btns'>
+                                        <div className='col-3 p-0 actions-list'>
+                                            <span>
+                                                <svg stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 24 24" className="BiDotsHorizontalRounded" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" /></svg>
+                                                <div className='select-clint'>
+                                                    <Link to={`/restaurants/${user.id}`}>التفاصيل</Link>
+                                                    <Link to={`/shipping-companies/${user.user.id}`}>شركة الشحن</Link>
+                                                </div>
+                                            </span>
+                                        </div>
+                                        <div className='col-4 p-0 flex-center'>
+                                            <span className={user.status === 1 ? "green" : "red"}>{user.status === 1 ? "نشط" : "غير نشط"}</span>
+                                        </div>
+                                        <div className='col-5 p-0'>
+                                            <Link to={`/restaurants/${user.id}`} className="btn btn-main">التفاصيل</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
 
 
-        }
-    }
+    )
+
+    return (
+        <div className="main-table">
+            <SortTabel
+                setSortSearch={setSortSearch}
+                searchSort={searchSort}
+                HandelShowCustomer={HandelShowCustomer}
+                UserData={UserData}
+                setSortedField={setSortedField}
+                sortData={sortData}
+                sortID={sortID}
+                sortName={sortName}
+                sortDuration={sortDuration}
+                sortpaymentDate={sortpaymentDate}
+            />
+
+            <div className='gird-show'>
+                {dataRender}
+            </div>
+
+        </div>
+    )
 }
 
-
-.red{
-    color: var(--danger-color);
-    font-weight: bold;
-    background-color: #ff000015;
-    padding: 10px;
-    border-radius:30px ;
-        
-    }
-.green{
-    color: var(--succes-color);
-    font-weight: bold;
-    background-color: #00800015;
-    padding: 10px;
-    border-radius:30px ;
-}
-
-`
 
 
 export default TableAllUsers
