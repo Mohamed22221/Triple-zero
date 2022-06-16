60869d173313cdd6ddd6f67deb5cea0e9860442e
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-06-16 15:24:20 +0200

    Buttons-Add

--- src/components/bars/NotBar.js
+++ src/components/bars/NotBar.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import { Outlet } from 'react-router-dom';
+import OverLay from '../glopal/OverLay';
 import SideBar from "./../bars/SideBar";
 const SidebarLayout = () => {
 
     return (
         <>
           <SideBar  />
+          <OverLay />
           <Outlet />
         </>
 
