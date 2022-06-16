a1953deb0449305630a355d590050ca0c21e94c9
Mohamed Elkamary <mohamedelkamary5@gmail.com>
2022-06-16 15:13:48 +0200

    over lay

--- src/components/bars/NotBar.js
+++ src/components/bars/NotBar.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import { Outlet } from 'react-router-dom';
 import SideBar from "./../bars/SideBar";
+import OverLay from "./../glopal/OverLay";
 const SidebarLayout = () => {
 
     return (
         <>
+        <OverLay/>
           <SideBar  />
           <Outlet />
         </>
