ccafddf5471c08101471d35bb2eeaf73a6c108f4
Mohamed Elkamary <mohamedelkamary5@gmail.com>
2022-06-20 14:54:43 +0200

    done drinvers

--- src/components/drivers/AddForm.js
+++ src/components/drivers/AddForm.js
@@ -1,73 +1,72 @@
-import React ,{useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from "styled-components"
 import InputCustomer from './Inputs'
-import {  useDispatch ,useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import SliderClint from '../glopal/SliderClint';
 import { MdPersonAddAlt } from 'react-icons/md';
 import swal from 'sweetalert';
 import { HideSlider } from '../../store/StateSlice';
 import { SendDirver } from '../../store/DriverSlice';
 
-const ClintForm = ({setShow}) => {
+const ClintForm = ({ setShow }) => {
   const toogleslider = useSelector((state) => state.ShowAndHide.value.driver)
 
   //get date today
   const today = new Date();
-  const date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+  const date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
   //redux toolkit
   const dispatch = useDispatch()
 
   const initialState = {
-      photo: null,
-      user_id: 2,
-      en_name: "",
-      ar_name: "" ,
-      mobile: '' ,
-      telephone: '',
-      address: "",
-      email : "" ,
-      status : 1,
-      password: '',
-      lon:-5650,
-      lat:2365,
-      
+    photo: null,
+    user_id: 2,
+    en_name: "",
+    ar_name: "",
+    mobile: '',
+    telephone: '',
+    address: "",
+    email: "",
+    status: 1,
+    isOnline: 0,
+    password: '',
+
   }
 
   const [values, setValues] = useState(initialState)
-  const AddUser = () =>{
+  const AddUser = () => {
     dispatch(SendDirver(values))
-    .unwrap()
-    .then(() => {
-      setValues(initialState)
-      dispatch(HideSlider())
-      swal("تم تنفيذ الامر بنجاح", {
-        icon: "success",
-        button: 'موافق',
-      });
-    }).catch(() => {
-      swal("عفوا لم يتم تنفيذ الامر", {
-        icon: "error",
-        button: 'موافق'
-      });
+      .unwrap()
+      .then(() => {
+        setValues(initialState)
+        dispatch(HideSlider())
+        swal("تم تنفيذ الامر بنجاح", {
+          icon: "success",
+          button: 'موافق',
+        });
+      }).catch(() => {
+        swal("عفوا لم يتم تنفيذ الامر", {
+          icon: "error",
+          button: 'موافق'
+        });
 
-    })
+      })
 
     setShow(true)
   }
   return (
-    <StyleForm  toogleslider={toogleslider ? "true" : 'false'}>
+    <StyleForm toogleslider={toogleslider ? "true" : 'false'}>
       <div className='style-form' toogleslider={toogleslider ? "true" : 'false'}>
         <SliderClint title="اضافه سائق"   >
-          <InputCustomer values={values} setValues={setValues}  /> 
+          <InputCustomer values={values} setValues={setValues} />
         </SliderClint>
-      <StyleFotter>
-        <button onClick={AddUser} className="btn btn-main">
-          <MdPersonAddAlt className='MdPersonAddAlt' />
-          <span>اضافه سائق</span>
-        </button>
+        <StyleFotter>
+          <button onClick={AddUser} className="btn btn-main">
+            <MdPersonAddAlt className='MdPersonAddAlt' />
+            <span>اضافه سائق</span>
+          </button>
 
-        <span>تريد المساعدة ؟ <a href='#'>اضغط هنا</a></span>
-      </StyleFotter> 
+          <span>تريد المساعدة ؟ <a href='#'>اضغط هنا</a></span>
+        </StyleFotter>
       </div>
     </StyleForm>
   )
@@ -99,7 +98,7 @@ transform: ${(props) => JSON.parse(props.toogleslider) ? 'translateX(0)  ' : '  
 
 `
 
-const StyleFotter= styled.div`
+const StyleFotter = styled.div`
 display: flex;
 justify-content: center;
 flex-direction: column;
--- src/components/drivers/Inputs.js
+++ src/components/drivers/Inputs.js
@@ -1,239 +1,155 @@
+import React, { useState } from 'react'
 
-import { useDispatch, useSelector } from 'react-redux'
-import ShortUniqueId from 'short-unique-id'
-import React, { useState, useEffect } from 'react'
-import styled from "styled-components"
-import { SendDirver } from '../../store/DriverSlice';
-import { MdPersonAddAlt } from 'react-icons/md';
-import swal from 'sweetalert';
-import { HideSlider } from '../../store/StateSlice';
+import Switch from "react-switch";
+import UploadComponent from '../../Shared/Components/Upload/UploadComponent';
 
-
-const FormAddDeivers = ({ setDeleted, values, setValues }) => {
-    const ImgeHandeler = (e) => {
-        const Reader = new FileReader()
-        Reader.onload = () => {
-            if (Reader.readyState === 2) {
-                setValues({ ...values, photo: e.target.files[0] })
-            }
-        }
-        Reader.readAsDataURL(e.target.files[0])
-    }
-    //get date today
-    const today = new Date();
-    const date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
-    //redux toolkit
-    const dispatch = useDispatch()
-    //uniqe id
-    const uid = new ShortUniqueId({ length: 6 });
-
-    const initialState = {
-        photo: null,
-        user_id: 2,
-        en_name: "",
-        ar_name: "",
-        mobile: "",
-        address: "",
-        email: "",
-        status: 1,
-        isOnline: 0,
-        password: '',
-
-    }
-
-    //values input
-    // const [values, setValues] = useState(initialState)
-    // click add customer
-    const AddUser = () => {
-        dispatch(SendDirver(values))
-            .unwrap()
-            .then(() => {
-                setValues(initialState)
-                dispatch(HideSlider())
-                swal("تم تنفيذ الامر بنجاح", {
-                    icon: "success",
-                    button: 'موافق',
-                });
-            }).catch(() => {
-                swal("عفوا لم يتم تنفيذ الامر", {
-                    icon: "error",
-                    button: 'موافق'
-                });
-
+const FormAddShipping = ({ values, setValues }) => {
+    const [selectedFiles, setselectedFiles] = useState([]);
+    const handleAcceptedFiles = (files) => {
+        files.map(file =>
+            Object.assign(file, {
+                preview: URL.createObjectURL(file),
+                formattedSize: formatBytes(file.size)
             })
-
-
-        setDeleted(false)
-
+        );
+        setselectedFiles(files)
+        setValues({ ...values, photo: files[0] })
     }
+
+
+
+    const staustSwittch = (e) => {
+        if (values.status == 1) {
+            setValues({ ...values, status: 0 })
+        } else {
+            setValues({ ...values, status: 1 })
+        }
+    }
+    const staustOnlineSwittch = (e) => {
+        if (values.isOnline == 1) {
+            setValues({ ...values, isOnline: 0 })
+        } else {
+            setValues({ ...values, isOnline: 1 })
+        }
+    }
+
+    const formatBytes = (bytes, decimals = 2) => {
+        if (bytes === 0) return "0 Bytes";
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+    };
+
+    const valueSwitch = values.status == 1 ? true : false
+    const valueOnlineSwitch = values.isOnline == 1 ? true : false
+
     return (
-        <StyleMainInput>
-            <StyleForm>
-                <StyleSmaleDiv>
-                    <div className='one-input  '>
-                        <StyleLabel>رقم العميل</StyleLabel>
-                        <input type="text" placeholder='#As578' value={values.user_id} disabled />
+        <div className='main-input px-2'>
+            <div className='row'>
+                {/* Block Item */}
+                <div className='col-lg-12'>
+                    <div className="mb-3">
+                        <UploadComponent handleAcceptedFiles={handleAcceptedFiles} selectedFiles={selectedFiles} />
                     </div>
-
-
-                </StyleSmaleDiv>
-                <StyleSmaleDiv>
-                    <div className='one-input'>
-                        <StyleLabel>اضف شعار للعميل</StyleLabel>
-                        <input type="file" onChange={ImgeHandeler} />
+                </div>
+                {/* Block Item */}
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">الايميل<span>*</span> </label>
+                        <input type="email" className="form-control" id="email" placeholder="اكتب الايميل" value={values.email} required onChange={(e) => setValues({ ...values, email: e.target.value })} />
                     </div>
-
-                </StyleSmaleDiv>
-                <StyleSmaleDiv>
-                    <div className='one-input'>
-                        <StyleLabel>الاسم بالانجليزي <span>*</span></StyleLabel>
-                        <input type="text" placeholder='اكتب الاسم بالانجليزي' value={values.en_name} onChange={(e) => setValues({ ...values, en_name: e.target.value })} />
+                </div>
+                {/* Block Item */}
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="en_name" className="form-label">الاسم بالانجليزي<span>*</span> </label>
+                        <input type="text" className="form-control" id="en_name" placeholder="اكتب الاسم بالانجليزي" value={values.en_name} required onChange={(e) => setValues({ ...values, en_name: e.target.value })} />
                     </div>
-                    <div className='one-input'>
-                        <StyleLabel>الاسم بالعربي <span>*</span></StyleLabel>
-                        <input type="text" placeholder='اكتب الاسم بالعربي' value={values.ar_name} onChange={(e) => setValues({ ...values, ar_name: e.target.value })} />
+                </div>
+                {/* Block Item */}
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="ar_name" className="form-label">الاسم بالعربي<span>*</span> </label>
+                        <input type="text" className="form-control" id="ar_name" placeholder="اكتب الاسم بالعربي" value={values.ar_name} required onChange={(e) => setValues({ ...values, ar_name: e.target.value })} />
                     </div>
-
-                </StyleSmaleDiv>
-                <StyleSmaleDiv>
-                    <div className='one-input'>
-                        <StyleLabel>التليفون <span>*</span></StyleLabel>
-                        <input type="number" placeholder='اكتب التليفون' value={values.mobile} onChange={(e) => setValues({ ...values, mobile: e.target.value })} />
-
-
-                        <StyleLabel>ايميل السائق <span>*</span></StyleLabel>
-                        <input type="email" placeholder='اكتب اميل السائق' value={values.email} onChange={(e) => setValues({ ...values, email: e.target.value })} />
-                        <StyleLabel>  العنوان <span>*</span></StyleLabel>
-                        <input type="text" placeholder='اكتب العنوان ' value={values.address} onChange={(e) => setValues({ ...values, address: e.target.value })} />
-                        <StyleLabel>الحالة<span>*</span></StyleLabel>
-                        <input type="text" placeholder='اكتب الحالة' value={values.status} onChange={(e) => setValues({ ...values, status1: e.target.value })} />
-                        <StyleLabel>حالة الاتصال<span>*</span></StyleLabel>
-                        <input type="text" placeholder='اكتب حالة الاتصال' value={values.isOnline} onChange={(e) => setValues({ ...values, isOnline: e.target.value })} />
-                        <StyleLabel>كلمة السر<span>*</span></StyleLabel>
-                        <input type="text" placeholder='اكتب الحالة' value={values.password} onChange={(e) => setValues({ ...values, password: e.target.value })} />
-
+                </div>
+                {/* Block Item */}
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="mobile" className="form-label">التليفون<span>*</span> </label>
+                        <input type="number" className="form-control" id="mobile" placeholder="اكتب التليفون" value={values.mobile} required onChange={(e) => setValues({ ...values, mobile: e.target.value })} />
                     </div>
-                </StyleSmaleDiv>
+                </div>
+                {/* Block Item */}
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label"> كلة السر<span>*</span> </label>
+                        <input type="password" className="form-control" id="password" placeholder="اكتب  كلة السر" value={values.password} required onChange={(e) => setValues({ ...values, password: e.target.value })} />
+                    </div>
+                </div>
+                {/* Block Item */}
+                {/* <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="address" className="form-label"> العنوان<span>*</span> </label>
+                        <input type="text" className="form-control" id="address" placeholder="اكتب  العنوان" value={values.address} required onChange={(e) => setValues({ ...values, address: e.target.value })} />
+                    </div>
+                </div> */}
+                {/* Block Item */}
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="switch-add-shipping" className="form-label d-block">الحالة</label>
+                        <label className="switch-item" htmlFor='switch-add-shipping'>
+                            <Switch
+                                checked={valueSwitch}
+                                onChange={staustSwittch}
+                                id='switch-add-shipping'
+                                handleDiameter={28}
+                                offColor="#dfdcdc"
+                                onColor="#f7d294"
+                                offHandleColor="#707070"
+                                onHandleColor="#FB9E00"
+                                height={30}
+                                width={70}
+                                borderRadius={50}
+                                uncheckedIcon={false}
+                                checkedIcon={false}
+                                className="react-switch"
+                            />
+                        </label>
+                    </div>
+                </div>
+                <div className='col-lg-6'>
+                    <div className="mb-3">
+                        <label htmlFor="switch-add-shipping" className="form-label d-block"> الحالة الاتصال</label>
+                        <label className="switch-item" htmlFor='switch-add-shipping'>
+                            <Switch
+                                checked={valueOnlineSwitch}
+                                onChange={staustOnlineSwittch}
+                                id='switch-add-shipping'
+                                handleDiameter={28}
+                                offColor="#dfdcdc"
+                                onColor="#f7d294"
+                                offHandleColor="#707070"
+                                onHandleColor="#FB9E00"
+                                height={30}
+                                width={70}
+                                borderRadius={50}
+                                uncheckedIcon={false}
+                                checkedIcon={false}
+                                className="react-switch"
+                            />
+                        </label>
+                    </div>
+                </div>
 
-            </StyleForm>
-            {/* <StyleButtonAdd onClick={AddUser} >
-                <MdPersonAddAlt className='MdPersonAddAlt' />
-                <span > إضافة سائق </span>
-            </StyleButtonAdd> */}
-        </StyleMainInput>
+               
+            </div>
+
+        </div>
     )
 }
-const StyleMainInput = styled.div`
-overflow-y: auto;
-overflow-x: hidden;
-padding: 20px 0;
-margin-top: 30px;
-display: flex;
-flex-direction: column;
-align-items: center;
 
-::-webkit-scrollbar {
-  width: 0px;
-}
-`
-const StyleForm = styled.form`
-
-`
-const StyleLabel = styled.label`
-font-weight: bold;
-span{
-    color: var(--danger-color);
-}
-`
-const StyleSmaleDiv = styled.div`
-display: flex;
-align-items: center;
-@media (max-width:550px ) {
-    flex-direction: column;
-    width: 100%;
-    align-items: flex-start;
-}
-&:first-child{
-    opacity: 0.6;
-}
-&:nth-child(4){
-    input {
-    width: 470px;
-    @media (max-width:550px ) {
-    width:290px;
-    padding: 10px 30px;
-    }
-    @media (max-width:370px ) {
-    width:220px;
-    padding: 10px 30px;
-}
-
-    }
-}
-
-input,.currency{
-    font-size: 14px;
-    width: 230px;
-    margin: 8px 0 8px 10px;
-    padding: 8px 12px;
-    border: 1px solid var(--font-opacity);
-    border-radius: 20px;
-@media (max-width:550px ) {
-    width:290px;
-    padding: 10px 30px;
-}
-@media (max-width:370px ) {
-    width:220px;
-    padding: 10px 30px;
-}
-}
-input[type=file] {
-background-color: var(--secound-color);
-color: white;
-opacity: 0.9;
-}
-//class
-.one-input{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-}
-.currency{
-    font-weight: bold;
-}
-
-
-
-`
-
-
-const StyleButtonAdd = styled.div`
-display: flex;
-align-items: center;
-background-color: var(--primary-color);
-padding: 8px 20px;
-width: 166px;
-text-align: center;
-cursor: pointer;
-color: white;
-margin-top: 20px;
-border-radius: 7px;
-transition: 0.6s;
-display: flex;
-justify-content: space-evenly;
-:hover{
-  opacity: 0.7;
-}
-p{
-    padding: 0 10px ;
-}
-.MdPersonAddAlt{
-    font-size: 25px;
-}
-@media (max-width:1160px) {  
-margin: 10px;
-           
-}
-`
-export default FormAddDeivers
+export default FormAddShipping
